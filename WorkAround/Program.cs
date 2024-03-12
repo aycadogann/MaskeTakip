c6c4ddfaf197368ae39c126194283c66ad8594a4
Ayca Dogan <dogan.aycaa@gmail.com>
2024-03-12 21:36:46 +0300

    Add project files.

---
+++ WorkAround/Program.cs
@@ -0,0 +1,27 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace WorkAround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "A";
+            person1.LastName = "D";
+            person1.DateOfBirthYear = 1991;
+            person1.NationalIdentity = 123;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+        }
+
+        static void SelamVer()
+        {
+            Console.WriteLine("Merhaba");
+        }
+    }
+
+}
